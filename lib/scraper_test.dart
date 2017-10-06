cc8dfaeaf23f89efdcf10a3ee1fe7dc667c2ea8e
Joel Trottier-Hebert <iamquebecois@gmail.com>
2017-10-06 19:29:58 -0400

    Out to json, with event scraped

--- lib/scraper_test.dart
+++ lib/scraper_test.dart
@@ -2,30 +2,65 @@
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
 import 'package:html/dom.dart';
 import 'package:http/http.dart';
 import 'package:html/parser.dart';
 
 class Event {
   String imgUrl;
+  String date;
+  String time;
+  String location;
+  String name;
+
+  Map toJson() => {
+        'imgUrl': imgUrl,
+        'date': date,
+        'time': time,
+        'location': location,
+        'name': name,
+      };
 }
 
 Future<Null> scrapeShows() async {
-
   var client = new Client();
 
-  var response = await client.get('http://diffusion.saguenay.ca/programmation/humour_varietes/');
+  var response = await client
+      .get('http://diffusion.saguenay.ca/programmation/humour_varietes/');
 
   var body = response.body;
   var parsedBody = parse(body);
-  
-  var eventElement = parsedBody.querySelectorAll("ul.event-grid > li.event")[0]; // .map((Element e) => new Event());
 
-  var imgElement = eventElement.querySelector('img');
-  var imageUrl = imgElement?.attributes['src'];
+  var events = parsedBody
+      .querySelectorAll("ul.event-grid > li.event")
+      .map((Element eventElement) {
+    var imageUrl = eventElement.querySelector('img').attributes['src'];
 
-  var event = new Event()..imgUrl = imageUrl;
+    var date =
+        eventElement.querySelector('.wrap-time > time').attributes['datetime'];
+    var time = eventElement.querySelector('.wrap-time > time > .time').text;
 
+    var location = eventElement.querySelector('.location').text;
 
-  print('Hello');
+    var name = eventElement
+        .querySelector('.main-block > h2 > a')
+        .text
+        .replaceAll('\n', '')
+        .replaceAll('\t', '');
+
+    var event = new Event()
+      ..name = name
+      ..imgUrl = imageUrl
+      ..date = date
+      ..time = time
+      ..location = location;
+
+    return event;
+  }).toList();
+
+  var out = new File('out.json');
+
+  out.writeAsStringSync(JSON.encode(events));
 }
