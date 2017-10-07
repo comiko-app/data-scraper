385195a5b1e1b02d1726ed478389da00ccd12f3f
Joel Trottier-Hebert <iamquebecois@gmail.com>
2017-10-06 22:29:49 -0400

    Rename

--- bin/main.dart
+++ bin/main.dart
@@ -1,7 +1,7 @@
 // Copyright (c) 2017, joel. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-import 'package:scraper_test/scraper_test.dart' as scraper_test;
+import 'package:scraper_poc/scraper.dart' as scraper_test;
 
 main(List<String> arguments) async {
   await scraper_test.scrapeShows();
